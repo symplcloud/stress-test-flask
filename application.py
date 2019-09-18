c44f6349377937a76a7ffa4c42ef660a4e8f1569
Sean Lee <seanlee@sympl.cloud>
2019-09-18 17:53:35 +0000

    run Fibonacci function directly from application.py

--- application.py
+++ application.py
@@ -1,23 +1,23 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-def Fibonacci(n): 
-    if n<0: 
-        print("Incorrect input") 
-    # First Fibonacci number is 0 
-    elif n==1: 
+@app.route('/')
+def hello():
+    return str(Fibonacci(20))
+
+def Fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==1:
         return 0
-    # Second Fibonacci number is 1 
-    elif n==2: 
+    # Second Fibonacci number is 1
+    elif n==2:
         return 1
-    else: 
+    else:
         return Fibonacci(n-1)+Fibonacci(n-2)
 
-@app.route('/')
-def index():
-    return str(Fibonacci(100))
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-        
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
